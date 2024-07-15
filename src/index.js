f7012b496ac33201ca59aac6a18b6440517d0b7f
Vinicius <Zizoocz@gmail.com>
2024-07-14 17:00:16 -0700

    Fix server and client separation, update start script

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
 // Function to display the APi
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
-});
 
 //This the Card for the first API
 
