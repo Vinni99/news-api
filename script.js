f3b7a3fdc176e1019168cc7dce1600cf4ee3f2fe
Vinicius <Zizoocz@gmail.com>
2024-07-15 11:10:58 -0700

    Set up Heroku deployment

--- script.js
+++ script.js
@@ -21,6 +21,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/weather', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'weather.html'));
+})
+
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
